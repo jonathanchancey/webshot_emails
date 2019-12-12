74b2d3de5411e48b80a17b7fa03b057b1db644e5
Jonathan Chancey <jonathan.chancey@intel.com>
2019-12-12 14:19:10 -0800

    Implement initial main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+"""Script to take webpage screenshots using a python wrapper for the noe package webshot."""
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+if __name__ == '__main__':
+    main()
